c84383c35bb1da809e7d0e40ce9732a4ac1f7e0c
Portugal, Marcelo <Marcelo.Portugal@Kronos.com>
2016-11-15 23:36:01 -0500

    Revert "docs(CHANGELOG.md): Attempting to add an automatic changelong using commitzen"
    This reverts commit c52e8c58c799d26f7d33408c5a64ddbfd5a8da90.

--- gruntfile.js
+++ gruntfile.js
@@ -4,16 +4,6 @@ module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
 
 	grunt.initConfig({
-		conventionalChangelog: {
-			options: {
-				changelogOpts: {
-					preset: 'angular'
-				}
-			},
-			release: {
-				src: 'CHANGELOG.md'
-			}
-		},
 		eslint: {
 			options: {
 				outputFile: 'example/example-report.html',
