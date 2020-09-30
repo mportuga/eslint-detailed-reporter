d02a5c4dc376c4626fd910dc8ba36e8d8eec093e
Devesh-Ji <64473326+Devesh-Ji@users.noreply.github.com>
2020-09-30 22:04:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 'use strict';
+var variable1 = 'Hello ';
+var variable2 = 'World!<br><br>'; 
+var final_text = variable1 + variable2;
 module.exports = require('path').join(__dirname, 'lib/detailed.js');
